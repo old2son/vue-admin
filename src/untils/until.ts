10589bb37ec73eb1f379bdf2171f7ed77723b011
old2son <caiye2531889700@gmail.com>
2023-02-10 22:42:23 +0800

    untils modify

--- src/untils/until.ts
+++ src/untils/until.ts
@@ -34,3 +34,21 @@ export function localSet(key: string, value: any) {
 export function localRemove() {
     window.localStorage.clear();
 }
+
+/**
+ * @description 判断数据类型
+ * @param {Any} val 需要判断类型的数据
+ * @return string
+ */
+export function isType(val: any) {
+    if (val === null) {
+        return 'null';
+    }
+
+    if (typeof val !== 'object') {
+        return typeof val;
+    }
+    else {
+        return Object.prototype.toString.call(val).slice(8, -1).toLocaleLowerCase();
+    }
+}

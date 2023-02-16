80097e5fb774a097a4436e78734f2d0af2eafe40
old2son <caiye2531889700@gmail.com>
2023-02-17 00:17:51 +0800

    isObjectValueEuqal

--- src/untils/until.ts
+++ src/untils/until.ts
@@ -84,3 +84,49 @@ export function generateUUID() {
 		return (c === 'x' ? random : (random & 0x3) | 0x8).toString(16);
 	});
 }
+
+/**
+ * 判断两个对象是否相同
+ * @param a 要比较的对象一
+ * @param b 要比较的对象二
+ * @returns 相同返回 true，反之则反
+ */
+export function isObjectValueEuqal(a: { [key: string]:any }, b: { [key: string]: any }) {
+    if (!a || !b) {
+        return false;
+    }
+
+    let aProps = Object.getOwnPropertyNames(a);
+    let bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (let i = 0; i < aProps.length; i++) {
+        let propName = aProps[i];
+        let propA = a[propName];
+        let propB = b[propName];
+
+        // if (!b.hasOwnProperty(propName)) {return false;} // Do not access Object.prototype method 'hasOwnProperty'
+        if (!Object.prototype.hasOwnProperty.call(b, propName)) {
+            return false;
+        }
+
+        if (propA instanceof Object) {
+            if (!isObjectValueEuqal(propA, propB)) {
+                return false;
+            } 
+        }
+        else if (propA !== propB) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+
+
